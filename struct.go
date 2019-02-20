5f1ffe61f5629e7ee344288869f09163f6407d34
A.R.I <mt.mehrab@gmail.com>
2019-02-20 14:08:32 +0330

    working with strutcs and methods

---
+++ struct.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
+
+type car struct {
+	gasPedal      uint16
+	brakePedal    uint16
+	steeringWheel int16
+	topSpeedKmh   float64
+}
+
+func (c car) kmh() float64{
+	return float64(c.gasPedal) * (c.topSpeedKmh/usixteenbitmax)
+}
+
+func (c car) mph() float64{
+	return float64(c.gasPedal) * (c.topSpeedKmh/usixteenbitmax/kmh_multiple)
+}
+
+func main() {
+	a_car := car{
+		gasPedal:      22341,
+		brakePedal:    0,
+		steeringWheel: 12561,
+		topSpeedKmh:   225.0}
+
+	fmt.Println(a_car)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
+}
