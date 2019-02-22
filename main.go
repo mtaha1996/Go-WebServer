13191563c67246df00ed8f4efa639d8bc6363a8c
Mohammad Taha Mehrabbeygi <mt.mehrab@gmail.com>
2019-02-22 11:36:22 +0330

    creating a simple html page with html/template

--- main.go
+++ main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type NewsAggPage struct {
+	Title string
+	News  string
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Whoa, Go is neat!")
+	_, _ = fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "Export web design by TaHa")
 }
 
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing news aggregator", News: "some news"}
+	t, _ := template.ParseFiles("basictemplating.html")
+	t.Execute(w, p)
+
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/about/", aboutHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
 
 	_ = http.ListenAndServe(":8000", nil)
 
--- web_scraper.go
+++ web_scraper.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -24,24 +25,28 @@ type SiteMapIndex struct {
 }
 
 type News struct {
-	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>Keywords"`
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
 }
 
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
 
 func main() {
 	var s SiteMapIndex
 	var n News
+	news_map := make(map[string]NewsMap)
 
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	//stringBody := string(bytes)
 	//fmt.Println(stringBody)
-	resp.Body.Close()
+	_ = resp.Body.Close()
 
-
-	xml.Unmarshal(bytes, &s)
+	_ = xml.Unmarshal(bytes, &s)
 
 	//fmt.Println(s.Locations)
 
@@ -50,12 +55,18 @@ func main() {
 
 		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
+		_ = xml.Unmarshal(bytes, &n)
 
-		resp.Body.Close()
-
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+		_ = resp.Body.Close()
 	}
 
-
+	for idx, data := range news_map {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+	}
 
 }
