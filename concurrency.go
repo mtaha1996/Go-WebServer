e885a0cdec7b39226013b20708dfa47d4b7b4343
Mohammad Taha Mehrabbeygi <mt.mehrab@gmail.com>
2019-02-22 16:58:14 +0330

    Panic and Recover!

--- concurrency.go
+++ concurrency.go
@@ -8,12 +8,22 @@ import (
 
 var wg sync.WaitGroup
 
+func cleanup() {
+	if r := recover(); r != nil {
+		fmt.Println("Recover in cleanup", r)
+	}
+	wg.Done()
+}
+
 func say(s string) {
+	defer cleanup()
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Microsecond * 100)
+		if i == 2 {
+			panic("Oh dear! a 2")
+		}
 	}
-	wg.Done()
 }
 
 func main() {
@@ -22,4 +32,5 @@ func main() {
 	wg.Add(1)
 	go say("there!")
 	wg.Wait()
+
 }
